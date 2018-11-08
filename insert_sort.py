5c1f5b03483af20779dec6e043b0b1d7df0ddeef
henry199101 <331243543@qq.com>
2018-11-08 16:16:46 +0800

    改善插入排序的可读性。

--- insert_sort.py
+++ insert_sort.py
@@ -1,8 +1,16 @@
 def insert_sort(nums):
-	for i in range(1, len(nums)):
-		itemToInsert = nums[i]
+	i = 1
+	n = len(nums)
+
+	while i <= n-1:
 		j = i
-		while j>=1 and itemToInsert<nums[j-1]:
+		itemToInsert = nums[i]
+
+		while j >= 1 and itemToInsert < nums[j-1]:
+
 			nums[j] = nums[j-1]
 			j -= 1
+
 		nums[j] = itemToInsert
+
+		i += 1
