d0e298e872c19e22bd8543ecd04d9a4410957d8b
henry199101 <331243543@qq.com>
2018-11-08 14:51:14 +0800

    改善冒泡排序的可读性。

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,23 +1,29 @@
 from swap import swap
 
 def bubble_sort(nums):
+	n = len(nums)
 
-	for i in range(len(nums)-1):
-		for j in range(0, len(nums)-i-1):
-			if nums[j] > nums[j+1]:
-				swap(nums, j, j+1)
+	for i in range(n-1):
+		j = 1
+		while j <= n-i-1:
+			if nums[j-1] > nums[j]:
+				swap(nums, j-1, j)
+			j += 1
 
 
 def bubble_sort_better(nums):
+	n = len(nums)
 
-	for i in range(len(nums)-1):
+	for i in range(n-1):
 		
 		flag = False
 		# flag 用于标记内层循环中，是否发生了交换。		
-		for j in range(0, len(nums)-i-1):
-			if nums[j] > nums[j+1]:
-				swap(nums, j, j+1)
+		j = 1
+		while j <= n-i-1:
+			if nums[j-1] > nums[j]:
+				swap(nums, j-1, j)
 				flag = True	# 发生了交换。
+			j += 1
 
 		if flag==False:	break
 		# 若未发生交换，则说明数列初始状态是从小到大排列的，即有序的。
